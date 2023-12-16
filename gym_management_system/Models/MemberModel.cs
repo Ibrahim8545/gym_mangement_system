76294c23e211867f70c6aa40e9b9da9eb10c688e
Adham Elagaty <adhamalagaty@gmail.com>
2023-12-16 20:13:04 +0200

    create member model

---
+++ gym_management_system/Models/MemberModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gym_management_system.Models
+{
+    public class MemberModel : PersonModel
+    {
+        private int attendanceCount;
+        public MemberModel(int id = 0, string firstName = null, string secondName = null, string gender = null, string email = null, string phoneNumber = null, DateTime brithday = default, Image picture = null, string base64Image = null, int attendanceCount = 0) : base(id: id, firstName: firstName, secondName: secondName, gender: gender, email: email, phoneNumber: phoneNumber, brithday: brithday, picture: picture)
+        {
+            AttendanceCount = attendanceCount;
+        }
+        public int AttendanceCount { get { return attendanceCount; } set { attendanceCount = value; } }
+        public override int generateId()
+        {
+            int id;
+            double currentYear = DateTime.Now.Year;
+            currentYear = (((currentYear % 100) / 100.0) + 2) * 1000000;
+            int y = (int)currentYear;
+            int last_id = 0;
+            if (last_id == -1)
+            {
+                return -1;
+            }
+            if (last_id == 0 || (last_id / 1000000) != (y / 1000000))
+            {
+                id = ++y;
+            }
+            else
+            {
+                id = last_id + 1;
+            }
+            this.id = id;
+            return id;
+        }
+    }
+}
