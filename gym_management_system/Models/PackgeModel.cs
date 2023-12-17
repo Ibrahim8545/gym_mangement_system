ad611ed82de08c849fd5bc5fd9487c769da6f738
Adham Elagaty <adhamalagaty@gmail.com>
2023-12-17 17:02:11 +0200

    create packgeModel

---
+++ gym_management_system/Models/PackgeModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gym_management_system.Models
+{
+    public class PackgeModel
+    {
+        private int id, numOfClass, numOfInvatation;
+        private string name, status;
+        private MonthOfferModel monthOffer;
+
+        public PackgeModel(int id = 0, int numOfClass = 0, int numOfInvatation = 0, string name = null, string status = null, MonthOfferModel monthOffer = null)
+        {
+            Id = id;
+            NumOfClass = numOfClass;
+            NumOfInvatation = numOfInvatation;
+            Name = name;
+            Status = status;
+            MonthOffer = monthOffer;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int NumOfClass { get => numOfClass; set => numOfClass = value; }
+        public int NumOfInvatation { get => numOfInvatation; set => numOfInvatation = value; }
+        public string Name { get => name; set => name = value; }
+        public string Status { get => status; set => status = value; }
+        public MonthOfferModel MonthOffer { get => monthOffer; set => monthOffer = value; }
+    }
+}
