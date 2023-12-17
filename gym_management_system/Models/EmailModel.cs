853ea417d0bf582d944404cd4aed30a048a61119
Adham Elagaty <adhamalagaty@gmail.com>
2023-12-17 20:31:44 +0200

    create EmailModel and edit on MemberEmail model

---
+++ gym_management_system/Models/EmailModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gym_management_system.Models
+{
+    public abstract class EmailModel
+    {
+        protected int id;
+        protected string subject;
+        protected DateTime date;
+        protected EmployeeModel employeeModel;
+
+        protected EmailModel(int id = 0, string subject = null, DateTime date = default, EmployeeModel employeeModel = null) { 
+            Id = id;
+            Subject = subject;
+            Date = date;
+            EmployeeModel = employeeModel;
+        }
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Subject { get { return subject; } set { subject = value; } }
+        public DateTime Date { get { return date; } set { date = value; } }
+        public EmployeeModel EmployeeModel { get { return employeeModel; } set { employeeModel = value; } }
+
+        public EmployeeModel getEmployeeData()
+        {
+            List<EmployeeModel> employeeModels = Global.employeeService.Search(employeeModel.Id.ToString(), false, byId: false);
+            if (employeeModels != null)
+            {
+                employeeModel = employeeModels[0];
+                return employeeModel;
+            }
+            else
+            {
+                Console.WriteLine("Error getting from getEmployeeData in Email Model: no employee found");
+                return null;
+            }
+        }
+
+        public abstract PersonModel getreciverData();
+    }
+}
--- gym_management_system/Models/MemberEmailModel.cs
+++ gym_management_system/Models/MemberEmailModel.cs
@@ -6,27 +6,30 @@ using System.Threading.Tasks;
 
 namespace gym_management_system.Models
 {
-    public class MemberEmailModel
+    public class MemberEmailModel : EmailModel
     {
-        private int id;
-        private string subject;
-        DateTime date;
-        MemberModel memberModel;
-        EmployeeModel employeeModel;
+        private MemberModel memberModel;
 
-        public MemberEmailModel(int id = 0, string subject = null, DateTime date = default, MemberModel memberModel = null, EmployeeModel employeeModel = null)
+        public MemberEmailModel(int id = 0, string subject = null, DateTime date = default, MemberModel memberModel = null, EmployeeModel employeeModel = null) : base(id,subject,date,employeeModel)
         {
-            Id = id;
-            Subject = subject;
-            Date = date;
             MemberModel = memberModel;
-            EmployeeModel = employeeModel;
         }
 
-        public int Id { get { return id; } set { id = value; } }
-        public string Subject { get { return subject; } set { subject = value; } }
-        public DateTime Date { get { return date; } set { date = value; } }
         public MemberModel MemberModel { get {  return memberModel; } set {  memberModel = value; } }
-        public EmployeeModel EmployeeModel { get { return employeeModel; } set {  employeeModel = value; } }
+
+        public override PersonModel getreciverData()
+        {
+            List<MemberModel> memberModels = Global.memberService.Search(memberModel.Id.ToString(), false, byId: false);
+            if (memberModels != null)
+            {
+                memberModel = memberModels[0];
+                return memberModel;
+            }
+            else
+            {
+                Console.WriteLine("Error getting from getMemberData in MemberEmail Model: no member found");
+                return null;
+            }
+        }
     }
 }
