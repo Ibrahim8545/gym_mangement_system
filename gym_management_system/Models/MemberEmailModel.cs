72dd54c63717ba06aa31faa275b0530f569c0145
Adham Elagaty <adhamalagaty@gmail.com>
2023-12-17 17:45:14 +0200

    create member email model

---
+++ gym_management_system/Models/MemberEmailModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gym_management_system.Models
+{
+    public class MemberEmailModel
+    {
+        private int id;
+        private string subject;
+        DateTime date;
+        MemberModel memberModel;
+        EmployeeModel employeeModel;
+
+        public MemberEmailModel(int id = 0, string subject = null, DateTime date = default, MemberModel memberModel = null, EmployeeModel employeeModel = null)
+        {
+            Id = id;
+            Subject = subject;
+            Date = date;
+            MemberModel = memberModel;
+            EmployeeModel = employeeModel;
+        }
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Subject { get { return subject; } set { subject = value; } }
+        public DateTime Date { get { return date; } set { date = value; } }
+        public MemberModel MemberModel { get {  return memberModel; } set {  memberModel = value; } }
+        public EmployeeModel EmployeeModel { get { return employeeModel; } set {  employeeModel = value; } }
+    }
+}
