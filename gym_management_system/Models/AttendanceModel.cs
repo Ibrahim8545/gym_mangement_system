f67587851ff9291f37a8c2c8721a59179f7aa1ec
Adham Elagaty <adhamalagaty@gmail.com>
2023-12-17 17:11:15 +0200

    create Attendance Model as abstract class

---
+++ gym_management_system/Models/AttendanceModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gym_management_system.Models
+{
+    public abstract class AttendanceModel
+    {
+        protected int id;
+        protected DateTime date;
+
+        protected AttendanceModel(int id = 0, DateTime date = default)
+        {
+            Id = id;
+            Date = date;
+        }
+
+        public DateTime Date { get => date; set => date = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
