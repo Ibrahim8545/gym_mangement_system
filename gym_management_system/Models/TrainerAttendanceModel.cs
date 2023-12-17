b299bb39823bb89e6595f903701cfa644a46793c
Adham Elagaty <adhamalagaty@gmail.com>
2023-12-17 17:54:37 +0200

    edit on TrainerAttendanceModel

--- gym_management_system/Models/TrainerAttendanceModel.cs
+++ gym_management_system/Models/TrainerAttendanceModel.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace gym_management_system.Models
 {
-    internal class TrainerAttendanceModel
+    public class TrainerAttendanceModel : AttendanceModel
     {
+        private TrainerModel trainerModel;
+
+        public TrainerAttendanceModel(int id = 0, DateTime date = default, TrainerModel trainerModel = null) : base(id, date)
+        {
+            TrainerModel = trainerModel;
+        }
+
+        public TrainerModel TrainerModel { get { return trainerModel; }  set { trainerModel = value; } }
+
+        public TrainerModel getTrainerData()
+        {
+            List<TrainerModel> trainerModels = Global.trainerService.Search(trainerModel.Id.ToString(), false, byId: false);
+            if (trainerModel != null)
+            {
+                trainerModel = trainerModels[0];
+                return trainerModel;
+            }
+            else
+            {
+                Console.WriteLine("Error getting from getTrainerData in TrainerAttendance model: no trainer found");
+                return null;
+            }
+        }
     }
 }
